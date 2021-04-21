280a4f2950d4b4aa10919dbc4b11c1f9ae68f248
Negi97Mohit <mohit.snegi97@gmail.com>
2021-04-21 13:00:23 -0400

    Added Java Files and created Jframe And Jpanels

--- src/Business/Role/LabAssistantRole.java
+++ src/Business/Role/LabAssistantRole.java
@@ -8,7 +8,7 @@ import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
-import userInterface.LabAssistantRole.LabAssistantWorkAreaJPanel;
+import userInterface.LabAssistantRole.LabWorkAreaJPanel;
 import javax.swing.JPanel;
 
 /**
@@ -19,7 +19,7 @@ public class LabAssistantRole extends Role {
 
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
-        return new LabAssistantWorkAreaJPanel(userProcessContainer, account, organization, business);
+        return new LabWorkAreaJPanel(userProcessContainer, account, organization, business);
     }
     
 }
